5cc25926fd251ab2e8381efcd20747049a3845de
P0sseid0n <matheuspossidoniom@outlook.com>
2022-03-18 22:09:10 -0300

    Background done

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+   <div id="app">
+      <img src="@/assets/BackImage.svg" alt="Back" id="BackImage" />
+      <div>
+         <img src="@/assets/Logo.svg" alt="Necturnal Logo" />
+      </div>
+      <NuxtLayout>
+         <NuxtPage />
+      </NuxtLayout>
+   </div>
 </template>
+
+<style>
+* {
+   box-sizing: border-box;
+   padding: 0;
+   margin: 0;
+}
+#app {
+   height: 100vh;
+   background: #f8e5d6;
+
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+}
+
+#BackImage {
+   position: absolute;
+   top: 0;
+   left: 0;
+
+   height: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+   <div>olá</div>
+</template>
+
+<style scoped>
+</style>
