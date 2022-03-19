c33059d70c70ebd8bbf2c95089708bf4f4cc0610
P0sseid0n <matheuspossidoniom@outlook.com>
2022-03-19 11:19:49 -0300

    Correcting background image z-index

--- app.vue
+++ app.vue
@@ -1,28 +1,36 @@
 <template>
    <div id="app">
       <img src="@/assets/BackImage.svg" alt="Back" id="BackImage" />
-      <div>
-         <img src="@/assets/Logo.svg" alt="Necturnal Logo" />
-      </div>
+      <img src="@/assets/Logo.svg" alt="Necturnal Logo" />
       <NuxtLayout>
          <NuxtPage />
       </NuxtLayout>
    </div>
 </template>
 
-<style>
+<style >
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap");
+
 * {
    box-sizing: border-box;
    padding: 0;
    margin: 0;
+   font-family: "Montserrat", sans-serif;
 }
+
 #app {
    height: 100vh;
    background: #f8e5d6;
+   padding: 0 24px;
 
    display: flex;
    flex-direction: column;
    justify-content: center;
+   align-items: flex-start;
+}
+
+#app > *:not(img) {
+   z-index: 2;
 }
 
 #BackImage {
@@ -31,5 +39,6 @@
    left: 0;
 
    height: 100%;
+   z-index: 0;
 }
 </style>
