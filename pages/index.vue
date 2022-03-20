1d7269c8297a643d9857216bfd03adf68981c88f
P0sseid0n <matheuspossidoniom@outlook.com>
2022-03-20 09:39:22 -0300

    Change journal list to /journals

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,11 @@
 </script>
 
 <template>
-   <main>
-      <img src="@/assets/EmptyState.svg" alt="EmptyState Icon" />
-
-      <NuxtLink to="/create-journal">Criar diário</NuxtLink>
-   </main>
+   <div>
+      <h1>Olá</h1>
+      <NuxtLink to="/signin">Entrar</NuxtLink>
+   </div>
 </template>
 
 <style scoped>
-main {
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   height: 100%;
-}
-
-img {
-   margin-top: 128px;
-   margin-bottom: 48px;
-}
-
-a {
-   font-weight: 600;
-   font-size: 16px;
-
-   text-decoration-line: underline;
-
-   color: #834825;
-}
 </style>
---
+++ pages/journals/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+   <main>
+      <img src="@/assets/EmptyState.svg" alt="EmptyState Icon" />
+
+      <NuxtLink to="/create-journal">Criar diário</NuxtLink>
+   </main>
+</template>
+
+<style scoped>
+main {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   height: 100%;
+}
+
+img {
+   margin-top: 128px;
+   margin-bottom: 48px;
+}
+
+a {
+   font-weight: 600;
+   font-size: 16px;
+
+   text-decoration-line: underline;
+
+   color: #834825;
+}
+</style>
